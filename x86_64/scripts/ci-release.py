5beca9e1e7ad1eccee789010c57b568ac6e12115
Thomas Byr <tbouyer2000@gmail.com>
2022-05-22 00:10:14 +0200

    x86_64 specific instructions

--- x86_64/scripts/ci-release.py
+++ x86_64/scripts/ci-release.py
@@ -16,8 +16,7 @@ if "version" in released_version:
     print("Version " + crate_version + " already exists on crates.io")
 
 else:
-    print("Could not find version " + crate_version +
-          " on crates.io; creating a new release")
+    print("Could not find version " + crate_version + " on crates.io; creating a new release")
 
     print("  Running `cargo publish`")
     subprocess.run(["cargo", "publish"], check=True)
@@ -27,10 +26,8 @@ else:
     sha = subprocess.run(["git", "rev-parse", "HEAD"], check=True,
                          stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
     subprocess.run([
-        "gh", "api", "/repos/rust-osdev/x86_64/git/refs",
-        "-X", "POST", "-H", "Accept: application/vnd.github.v3+json",
-        "-F", "ref=refs/tags/" + tag_name,
-        "-F", "sha="+sha
+        "gh", "api", "/repos/rust-osdev/x86_64/git/refs", "-X", "POST", "-H",
+        "Accept: application/vnd.github.v3+json", "-F", "ref=refs/tags/" + tag_name, "-F", "sha=" + sha
     ])
 
     print("  Done")
