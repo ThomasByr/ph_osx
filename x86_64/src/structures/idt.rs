aff2b643dc5cc3794064249e633cb541f4ba17bc
Thomas Byr <tbouyer2000@gmail.com>
2022-05-26 21:33:03 +0200

    x86_64 specific instructions

--- x86_64/src/structures/idt.rs
+++ x86_64/src/structures/idt.rs
@@ -1,12 +1,3 @@
-// Copyright 2017 Philipp Oppermann. See the README.md
-// file at the top-level directory of this distribution.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 //! Provides types for the Interrupt Descriptor Table and its entries.
 //!
 //! # For the builds without the `abi_x86_interrupt` feature
